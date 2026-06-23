e1a01f0f68e3f4650a20e6ea208ae57439206cde
Madi-pycoder <perdunproektovic@gmail.com>
2026-06-23 12:05:53 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
-TOKEN = 'YOUR_TOKEN'
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
+DATABASE_URL = os.getenv("DATABASE_URL")
+REDIS_URL = os.getenv("REDIS_URL")
